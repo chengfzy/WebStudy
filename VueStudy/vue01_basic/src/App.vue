299e03f02e794f02db9d69d4f046aefab2289e11
Jeffery Cheng <chengcheng0829@gmail.com>
2023-05-17 23:22:21 +0800

    add v-html bind

--- VueStudy/vue01_basic/src/App.vue
+++ VueStudy/vue01_basic/src/App.vue
@@ -1,11 +1,18 @@
 <template>
+  <p>message: {{ message }}</p>
+
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+
   <button @click="increment">Count is: {{ count }}</button>
 </template>
 
-<script type="module">
+<script lang="ts">
 export default {
   data () {
     return {
+      message: 'Hello Vue!',
+      rawHtml: '<span style="color:red">This should be red. </span>',
       count: 0
     }
   },
